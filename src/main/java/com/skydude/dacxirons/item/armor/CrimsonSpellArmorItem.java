2519163a1c24d8819b199a287c8dad750a4a74c4
skydude-1234 <180607702+skydude-1234@users.noreply.github.com>
2025-06-23 19:00:06 -0400

    Imported files to git

--- src/main/java/com/skydude/dacxirons/item/armor/CrimsonSpellArmorItem.java
+++ src/main/java/com/skydude/dacxirons/item/armor/CrimsonSpellArmorItem.java
@@ -4,13 +4,18 @@ package com.skydude.dacxirons.item.armor;
 //This is crimsonspellarmoritem
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.jetbrains.annotations.Nullable;
 import com.skydude.dacxirons.registries.ItemRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -34,5 +39,27 @@ public class CrimsonSpellArmorItem extends ImbueabledacxironsArmor {
 
 
     }
+    @SubscribeEvent
+    public static void onLivingTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            Player player = event.player;
 
+            if (!player.level().isClientSide) {
+                MobEffect bleeding = BuiltInRegistries.MOB_EFFECT.get(new ResourceLocation("dungeons_and_combat", "bleeding"));
+
+                //    AttributeInstance swimSpeed = player.getAttribute(ForgeMod.SWIM_SPEED.get());
+                if (swimSpeed != null) {
+                    if (player.isInWater() && player.getItemBySlot(EquipmentSlot.FEET).getItem() == AquaItems.NEPTUNIUM_BOOTS.get()) {
+                        if (!swimSpeed.hasModifier(INCREASED_SWIM_SPEED)) {
+                            swimSpeed.addPermanentModifier(INCREASED_SWIM_SPEED);
+                        }
+                    } else {
+                        if (swimSpeed.hasModifier(INCREASED_SWIM_SPEED)) {
+                            swimSpeed.removeModifier(INCREASED_SWIM_SPEED);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
